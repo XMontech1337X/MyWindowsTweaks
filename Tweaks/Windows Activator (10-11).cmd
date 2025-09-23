db249d6357d324eac8afd12e818e0aef6e722211
XMontech1337X <nikolaysyndicate1777@mail.ru>
2025-09-24 04:30:28 +0700

    Update Windows Activator (10-11).cmd

--- Tweaks/Windows Activator (10-11).cmd
+++ Tweaks/Windows Activator (10-11).cmd
@@ -2,3 +2,4 @@
 :: Start Powershell Command -ExecutionPolicy Bypass
 echo Starting Activation Windows Script...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://get.activated.win | iex"
+exit
